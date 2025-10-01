6561d2563bb5b4c161122cd9527733a73721912b
eugene <langyue555@gmail.com>
2025-10-01 22:12:45 +0400

    增加了一个原型模式

---
+++ csharp/utils/Prototype/Eugene.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utils.Prototype
+{
+    internal class Eugene: IEugene
+    {
+        public string Laohu { get; set; } = "eugene";
+        public int Age { get; set; } = 18;
+        public int BreathsPerMinute { get; set; } = 20;
+        public IEugene Clone()
+        {
+            Eugene eugene = new Eugene();
+            eugene.Laohu = this.Laohu;
+            eugene.Age = this.Age;
+            eugene.BreathsPerMinute = this.BreathsPerMinute;
+            return (IEugene)eugene;
+        }
+        public IEugene EugeneMemberwiseClone()
+        {
+            return (IEugene)this.MemberwiseClone();
+        }
+    }
+}
---
+++ csharp/utils/Prototype/IEugene.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utils.Prototype
+{
+    public interface IEugene
+    {
+         IEugene Clone();
+        IEugene EugeneMemberwiseClone();
+    }
+}
